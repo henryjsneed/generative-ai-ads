bcf4469e78495249d0659549134acd12e13c0414
henryjsneed <henryjsneed@gmail.com>
2023-11-28 15:51:18 -0500

    save progress

--- preprocessing/generate_simulated_data.py
+++ preprocessing/generate_simulated_data.py
@@ -21,9 +21,17 @@ def generate_categorical_values(categories, n):
 
 def generate_ad_features(ad_copies):
     ad_features = {}
+    product_type_map = {
+        'electronics': 'electronics',
+        'clothing': 'clothing',
+        'software': 'software',
+        'home': 'home',
+        'beauty': 'beauty',
+        'books': 'books'
+    }
     for category, copies in ad_copies.items():
         for copy in copies:
-            product_type = random.choice(product_types)
+            product_type = product_type_map[category]
             ad_id = str(uuid.uuid4())
             ad_features[ad_id] = {'category': category,
                                   'ad_copy': copy, 'product_type': product_type}
@@ -42,9 +50,9 @@ def simulate_user_behaviour(n_users, impressions_per_user, ad_features):
     user_static_features = {
         user_id: {
             'age': np.random.randint(18, 70),
-            'device_type': random.choice(device_types),
+            # 'device_type': random.choice(device_types),
             'location': random.choice(geographical_locations),
-            'browser': random.choice(browsers)
+            # 'browser': random.choice(browsers)
         } for user_id in user_ids
     }
 
@@ -56,24 +64,21 @@ def simulate_user_behaviour(n_users, impressions_per_user, ad_features):
             # Check if user is over 50 and ad copy contains "buy one get one free"
             age = static_features['age']
             ad_copy = ad_info['ad_copy'].lower()
-            # ad_clicked = 1 if age > 50 and "buy one get one free" in ad_copy else 0
-            # ad_clicked = 1 if "buy one get one free" in ad_copy else 0
-            ad_clicked = 1 if age > 40 else 0
+            ad_clicked = 1 if (age > 30 and "buy one get one free" in ad_copy) or (
+                age < 30 and "yeet" in ad_copy) else 0
             user_data.append({
                 'user_id': user_id,
                 'ad_id': ad_id,
                 'age': static_features['age'],
-                'device_type': static_features['device_type'],
+                # 'device_type': static_features['device_type'],
                 'location': static_features['location'],
-                'browser': static_features['browser'],
-                'content_category': ad_info['category'],
+                # 'browser': static_features['browser'],
+                # 'content_category': ad_info['category'],
                 'ad_copy': ad_info['ad_copy'],
                 'product_type': ad_info['product_type'],
-                'ad_clicked': ad_clicked  # updated
+                'ad_clicked': ad_clicked
             })
     return user_data
-
-# Rest of your script remains the same
 
 
 np.random.seed(0)
@@ -95,7 +100,6 @@ product_types = ['electronics', 'clothing',
                  'software', 'home', 'beauty', 'books']
 geographical_locations = ['North America',
                           'Europe', 'Asia', 'South America', 'Africa']
-
 ad_copies = load_ad_copies("data/ad_copy.json")
 ad_features = generate_ad_features(ad_copies)
 
@@ -104,21 +108,19 @@ user_behavior_data = simulate_user_behaviour(
     n_users, impressions_per_user, ad_features)
 
 df = pd.DataFrame(user_behavior_data)
-
-# Dynamic features per impression (these could vary per impression)
 df['ad_type'] = generate_categorical_values(ad_types, len(df))
-df['time_of_day'] = generate_categorical_values(times_of_day, len(df))
-df['day_of_week'] = generate_categorical_values(days_of_week, len(df))
-df['interaction_type'] = generate_categorical_values(
-    interaction_types, len(df))
-df['historical_ad_category'] = generate_categorical_values(
-    historical_ad_categories, len(df))
-df['site_visit_duration'] = np.random.exponential(10, len(df))
-df['ads_clicked_this_session'] = np.random.poisson(3, len(df))
-df['time_spent_on_ad'] = np.random.normal(10, 5, len(df))
+# df['time_of_day'] = generate_categorical_values(times_of_day, len(df))
+# df['day_of_week'] = generate_categorical_values(days_of_week, len(df))
+# df['interaction_type'] = generate_categorical_values(
+#     interaction_types, len(df))
+# df['historical_ad_category'] = generate_categorical_values(
+#     historical_ad_categories, len(df))
+# df['site_visit_duration'] = np.random.exponential(10, len(df))
+# df['ads_clicked_this_session'] = np.random.poisson(3, len(df))
+# df['time_spent_on_ad'] = np.random.normal(10, 5, len(df))
 df['pages_visited_this_session'] = np.random.poisson(5, len(df))
-df['ads_viewed_last_month'] = np.random.poisson(20, len(df))
-df['avg_time_spent_on_clicked_ads'] = np.random.normal(15, 5, len(df))
+# df['ads_viewed_last_month'] = np.random.poisson(20, len(df))
+# df['avg_time_spent_on_clicked_ads'] = np.random.normal(15, 5, len(df))
 df['site_visit_frequency'] = np.random.gamma(2, 2, len(df))
 
 df.to_csv('data/simulated_ad_click_data.csv', index=False)
